62648eac8de32a72e4f8b66f5f57e51897abba67
chaeros <chpark6851@koreatech.ac.kr>
2025-06-17 23:59:17 +0900

    feat : @Preview와 @Composable을 통한 미리보기 생성

--- HappyBirthday/app/src/main/java/com/chaeros/happybirthday/MainActivity.kt
+++ HappyBirthday/app/src/main/java/com/chaeros/happybirthday/MainActivity.kt
@@ -38,10 +38,18 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
-@Preview(showBackground = true)
+// @Composable 어노테이션이 있는 함수는 반드시 첫 글자가 대문자인 파스칼 표기법 사용
+// Compose 함수 작명법은 아래와 같다
+// 1. UI 컴포넌트는 명사로 명명해야 한다. : DoneButton()
+// 2. 동사 또는 동사구 x : DrawTextField()
+// 3. 명사화된 전치사 x : TextFieldWithLink()
+// 4. 형용사 x : Bright()
+// 5. 부사 x : Outside()
+// 6. 형용사 + 명사 o : RoundIcon()
+@Preview(showBackground = true) // @Preview : Design 창에서 미리보기 할 때 사용, 실제 앱에는 반영 x
 @Composable
-fun GreetingPreview() {
+fun BirthdayCardPreview() {
     HappyBirthdayTheme {
-        Greeting("Android")
+        Greeting("Chaeros")
     }
 }
