201392176a1b33fffbe8aa6c713053409bd14e25
chaeros <chpark6851@koreatech.ac.kr>
2025-06-30 23:59:25 +0900

    feat : Tip Percentage 입력 기능 추가 - stateless composable인 EditNumberField 사용 - mutableStateOf, by, remember 키워드 사용을 통한 state 관리

--- TipCalculator/app/src/main/java/com/chaeros/tipcalculator/MainActivity.kt
+++ TipCalculator/app/src/main/java/com/chaeros/tipcalculator/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -33,6 +34,7 @@ import com.chaeros.tipcalculator.ui.theme.TipCalculatorTheme
 import java.text.NumberFormat
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -54,11 +56,21 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+// 상태를 가지고 있는 Composable을 stateful composable 이라고 하고,
+// 상태를 가지고 있지않은 Composable을 stateless composable 이라고 한다.
+// 아래에서 TipCalculatorCard는 amountInput과 tipInput을 state로 가지고 있기 때문에 stateful composable
+// EditNumberField는 state를 가지고 있지 않기 떄문에 stateless composable 이다.
+// mutableStateOf : 변경 가능하며 compose에 의해 관찰되는 상태
+// remember : recomposition 되어도 값을 유지함. -> 해당 값을 변경한 경우가 아니라면, 다른 이유로 recomposition해도 초기화하지 않고 이전 값을 가짐
+// by : mutableState 객체의 값을 가져오기 위해서는 원래 .value를 사용해야 하나, by를 통해 이를 생략 가능
+// [ex) val amount = amountInput.value.toDoubleOrNull() ?: 0.0 -> val amount = amountInput.toDoubleOrNull() ?: 0.0]
 @Composable
 fun TipCalculatorCard(){
     var amountInput by remember { mutableStateOf("") }
-    val amount = amountInput.toDoubleOrNull() ?: 0.0
-    val tip = calculateTip(amount)
+    val amount = amountInput.toDoubleOrNull() ?: 0.0    // amountInput 변경 시, 자동 재계산 되며 recompose
+    var tipInput by remember { mutableStateOf("") }
+    val tipPercent = tipInput.toDoubleOrNull() ?: 0.0   // tipInput 변경 시, 자동 재계산 되며 recompose
+    val tip = calculateTip(amount,tipPercent)
 
     Column(
         modifier = Modifier
@@ -72,10 +84,22 @@ fun TipCalculatorCard(){
             modifier = Modifier
                 .align(alignment = Alignment.Start)
         )
+        // 지불 금액 입력 TextField
         EditNumberField(
+            label = R.string.bill_amount,
             value = amountInput,
             onValueChange = {amountInput=it},
             modifier = Modifier
+                .padding(bottom=30.dp)
+                .fillMaxWidth()
+        )
+        // Tip Percentage 입력 TextField
+        EditNumberField(
+            label = R.string.how_was_the_service,
+            value = tipInput,
+            onValueChange = {tipInput=it},
+            modifier = Modifier
+                .padding(bottom=30.dp)
                 .fillMaxWidth()
         )
         Text(
@@ -86,7 +110,6 @@ fun TipCalculatorCard(){
     }
 }
 
-//
 private fun calculateTip(amount: Double, tipPercent: Double = 15.0): String {
     val tip = tipPercent / 100 * amount
     return NumberFormat.getCurrencyInstance().format(tip)   // 현재 지역의 화폐 단위로 수를 반환
@@ -94,6 +117,7 @@ private fun calculateTip(amount: Double, tipPercent: Double = 15.0): String {
 
 @Composable
 fun EditNumberField(
+    @StringRes label: Int,  // @StringRes : 매개변수가 반드시 문자열 리소스를 참조하여야 함
     value: String,
     onValueChange: (String) -> Unit,
     modifier: Modifier = Modifier
@@ -102,8 +126,16 @@ fun EditNumberField(
         value = value,                      // 초기 입력 값
         onValueChange = onValueChange,      // 데이터 변화시 수행할 메서드
         singleLine = true,                  // 한 줄로만 보이도록, 길면 좌우 스크롤
-        label = {Text("Bill Amount")},  // Text Field에 입력해야 할 내용 설명
+        label = {Text(stringResource(label))},  // Text Field에 입력해야 할 내용 설명
         keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number), // 숫자만 입력 가능
         modifier = modifier
     )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TipCaculatorPreview(){
+    TipCalculatorTheme {
+        TipCalculatorCard()
+    }
 }
