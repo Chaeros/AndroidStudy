232cfa3cd14052089211f14df457cae3d236e538
chaeros <chpark6851@koreatech.ac.kr>
2025-07-04 23:59:47 +0900

    feat : Image Card Composable 추가 - 이미지 나타내주는 Card

---
+++ MyArtSpace/app/src/main/java/com/chaeros/myartspace/ui/common/ImageCard.kt
@@ -0,0 +1,22 @@
+package com.chaeros.myartspace.ui.common
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun ImageCard(
+    modifier: Modifier = Modifier,
+    painter: Painter,
+    imageDescription: String
+){
+    Image(
+        modifier = modifier
+            .padding(bottom = 20.dp),
+        painter = painter,
+        contentDescription = imageDescription
+    )
+}
