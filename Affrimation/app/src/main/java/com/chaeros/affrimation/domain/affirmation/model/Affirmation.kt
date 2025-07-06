a9555916aa9caae13fc4ad12246cd36ee11e510c
chaeros <chpark6851@koreatech.ac.kr>
2025-07-06 19:02:52 +0900

    feat : Affirmation data class 작성 및 관련 Data 리소스 추가
    - Affirmation data class 작성
    - Affirmation data List를 불러오는 Datasource class 작성
    - Affirmation 객체에 사용될 image 및 string 리소스 추가

---
+++ Affrimation/app/src/main/java/com/chaeros/affrimation/domain/affirmation/model/Affirmation.kt
@@ -0,0 +1,9 @@
+package com.chaeros.affrimation.domain.affirmation.model
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class Affirmation(
+    @StringRes val stringResourceId: Int,
+    @DrawableRes val imageResourceId: Int
+)
