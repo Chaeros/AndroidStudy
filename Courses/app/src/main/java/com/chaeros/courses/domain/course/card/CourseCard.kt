f1a1a7f4567daae4c0e2cdc42cc6e17560cf3e67
chaeros <chpark6851@koreatech.ac.kr>
2025-07-07 23:59:44 +0900

    feat : Course Card 추가
    - Card에 매개변수로 들어온 Topic 값에 따라 이미지, 관련 수업 개수, 수업 제목을 입력받아 출력함
    - Preview 기능을 통해 미리보기 가능

---
+++ Courses/app/src/main/java/com/chaeros/courses/domain/course/card/CourseCard.kt
@@ -0,0 +1,88 @@
+package com.chaeros.courses.domain.course.card
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.chaeros.courses.R
+import com.chaeros.courses.domain.course.data.DataSource
+import com.chaeros.courses.domain.course.model.Topic
+import com.chaeros.courses.ui.theme.Purple80
+
+@Composable
+fun CourseCard(
+    topic: Topic,
+    modifier: Modifier = Modifier
+){
+    Card(
+        shape = RoundedCornerShape(8.dp),
+        modifier = modifier
+            .padding(8.dp)
+    ) {
+        Row(
+            modifier = Modifier
+                .background(Purple80)
+        ) {
+            Image(
+                painter = painterResource(topic.imageResourceId),
+                contentDescription = stringResource(topic.stringResourceId),
+                modifier = Modifier
+                    .height(68.dp)
+                    .width(68.dp)
+            )
+            Column(
+                modifier = Modifier
+                    .background(Purple80)
+            ) {
+                Text(
+                    text = stringResource(topic.stringResourceId),
+                    modifier = Modifier
+                        .padding(start = 16.dp, end = 16.dp, top = 16.dp, bottom = 8.dp)
+                )
+                Row(
+                    horizontalArrangement = Arrangement.Center
+                ) {
+                    Icon(
+                        painter = painterResource(R.drawable.ic_grain),
+                        contentDescription = stringResource(R.string.ic_grain),
+                        modifier = Modifier
+                            .padding(start = 16.dp)
+                    )
+                    Text(
+                        text = topic.relatedCourseCount.toString(),
+                        modifier = Modifier
+                            .padding(start = 8.dp)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun CourseCardPreview(){
+    val list = DataSource.topics
+
+    LazyColumn {
+        items(list) { course ->
+            CourseCard(course)
+        }
+    }
+}
