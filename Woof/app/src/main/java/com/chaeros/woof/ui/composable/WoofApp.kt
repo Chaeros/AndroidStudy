451f4659ed3b2c9adc7c4471a0fa6c6a3af5b911
chaeros <chpark6851@koreatech.ac.kr>
2025-07-16 00:07:53 +0900

    feat: TopAppBar 생성 및 로고, 앱 이름 추가
    - Scaffold의 topBar에 WoofTopAppBar 컴포저블 적용
    - CenterAlignedTopAppBar 사용하여 상단 앱 바 구현
    - Row를 사용해 앱 로고 이미지와 텍스트 정렬
    - 이미지 크기와 패딩은 dimens.xml 리소스를 통해 설정
    - Row에 verticalAlignment 추가해 세로 중앙 정렬 개선

--- Woof/app/src/main/java/com/chaeros/woof/ui/composable/WoofApp.kt
+++ Woof/app/src/main/java/com/chaeros/woof/ui/composable/WoofApp.kt
@@ -3,6 +3,7 @@ package com.chaeros.woof.ui.composable
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
@@ -11,12 +12,18 @@ import com.chaeros.woof.data.dogs
 
 @Composable
 fun WoofApp() {
-    LazyColumn {
-        items(dogs) {
-            DogItem(
-                dog = it,
-                modifier = Modifier.padding(dimensionResource(R.dimen.padding_small))   // 내부 카드 간 padding 생성
-            )
+    Scaffold(
+        topBar = {
+            WoofTopAppBar()
+        }
+    ) { it ->
+        LazyColumn(contentPadding = it) {
+            items(dogs) {
+                DogItem(
+                    dog = it,
+                    modifier = Modifier.padding(dimensionResource(R.dimen.padding_small))   // 내부 카드 간 padding 생성
+                )
+            }
         }
     }
 }
