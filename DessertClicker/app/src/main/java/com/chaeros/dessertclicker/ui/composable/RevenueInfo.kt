aa5e514ed47c54bd8e1538bcbded8ccdc1cf9132
chaeros <chpark6851@koreatech.ac.kr>
2025-07-20 15:22:00 +0900

    feat: 디저트 판매량과 총 수익 정보 나타내는 UI 컴포저블 추가
    - TransactionInfo : 판매량과 수익을 수직으로 정렬
    - DessertsSoldInfo : 판매량 텍스트 표시 Row 구성
    - RevenueInfo : 총 수익 텍스트 표시 Row 구성
    - MaterialTheme 기반 스타일 적용 및 리소스 문자열 사용

---
+++ DessertClicker/app/src/main/java/com/chaeros/dessertclicker/ui/composable/RevenueInfo.kt
@@ -0,0 +1,32 @@
+package com.chaeros.dessertclicker.ui.composable
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import com.chaeros.dessertclicker.R
+
+// Activity에서 총 금액을 나타내는 줄
+@Composable
+fun RevenueInfo(revenue: Int, modifier: Modifier = Modifier) {
+    Row(
+        modifier = modifier,
+        horizontalArrangement = Arrangement.SpaceBetween,
+    ) {
+        Text(
+            text = stringResource(R.string.total_revenue),
+            style = MaterialTheme.typography.headlineMedium,
+            color = MaterialTheme.colorScheme.onSecondaryContainer
+        )
+        Text(
+            text = "$${revenue}",
+            textAlign = TextAlign.Right,
+            style = MaterialTheme.typography.headlineMedium,
+            color = MaterialTheme.colorScheme.onSecondaryContainer
+        )
+    }
+}
