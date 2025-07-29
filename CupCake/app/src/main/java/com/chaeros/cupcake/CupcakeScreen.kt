1ad97364834781bcca10100914ce3fe937db01ed
chaeros <chpark6851@koreatech.ac.kr>
2025-07-29 23:58:54 +0900

    Init : Compose를 사용하여 화면 간 이동 연습을 위한 CupCake Project upload

---
+++ CupCake/app/src/main/java/com/chaeros/cupcake/CupcakeScreen.kt
@@ -0,0 +1,65 @@
+package com.chaeros.cupcake
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.chaeros.cupcake.ui.OrderViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
+
+// TopAppBar와 TopAppBarDefaults.mediumTopAppBarColors()는 Material3의 실험적(Experimental) API로 아래 명시적 OptIn 설정이 필요
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CupcakeAppBar(
+    canNavigateBack: Boolean,
+    navigateUp: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    TopAppBar(
+        title = { Text(stringResource(id = R.string.app_name)) },
+        colors = TopAppBarDefaults.mediumTopAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer
+        ),
+        modifier = modifier,
+        navigationIcon = {
+            if (canNavigateBack) {
+                IconButton(onClick = navigateUp) {
+                    Icon(
+                        imageVector = Icons.Filled.ArrowBack,
+                        contentDescription = stringResource(R.string.back_button)
+                    )
+                }
+            }
+        }
+    )
+}
+
+@Composable
+fun CupcakeApp(
+    viewModel: OrderViewModel = viewModel(),
+    navController: NavHostController = rememberNavController()
+) {
+    Scaffold(
+        topBar = {
+            CupcakeAppBar(
+                canNavigateBack = false,
+                navigateUp = { /* TODO: implement back navigation */ }
+            )
+        }
+    ) { innerPadding ->
+        val uiState by viewModel.uiState.collectAsState()
+    }
+}
---
+++ CupCake/app/src/main/java/com/chaeros/cupcake/data/DataSource.kt
@@ -0,0 +1,19 @@
+package com.chaeros.cupcake.data
+
+import com.chaeros.cupcake.R
+
+object DataSource {
+    val flavors = listOf(
+        R.string.vanilla,
+        R.string.chocolate,
+        R.string.red_velvet,
+        R.string.salted_caramel,
+        R.string.coffee
+    )
+
+    val quantityOptions = listOf(
+        Pair(R.string.one_cupcake, 1),
+        Pair(R.string.six_cupcakes, 6),
+        Pair(R.string.twelve_cupcakes, 12)
+    )
+}
