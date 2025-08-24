fbe97397ed4a7e9edad464db1a360703ddc0dae2
chaeros <chpark6851@koreatech.ac.kr>
2025-08-24 20:09:13 +0900

    feat : Item Entity 생성
    - Room 활용을 위한 dependency 추가
    - data class에 Entity 어노테이션 적용을 통한 Room 테이블 생성
    - @PrimaryKey를 사용한 기본키 지정

--- Inventory/build.gradle.kts
+++ Inventory/build.gradle.kts
@@ -1,4 +1,11 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+// room_version 명시를 통해 build.gradle.kts dependency 에서 활용
+buildscript {
+    extra.apply {
+        set("room_version", "2.6.0")
+    }
+}
+
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
